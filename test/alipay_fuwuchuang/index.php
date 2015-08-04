c3c5f855aeb5885649d784e08fd3154fabeacb50
zhangfangxiong <zhangfangxiong@cric.com>
2015-08-04 17:18:01 +0800

    测试

--- test/alipay_fuwuchuang/index.php
+++ test/alipay_fuwuchuang/index.php
@@ -12,7 +12,6 @@ header ( "Content-type: text/html; charset=utf-8" );
  * 此文件是手机支付宝钱包访问商户网站的wap页面。商户的业务处理可以在本文件中处理。
  * 此文件的访问URL，请填入支付宝服务窗后台的菜单设置中，点击服务窗菜单即可访问到本文件。
  */
-
 // 日志记录下受到的请求
 writeLog ( "POST: " . var_export ( $_POST, true ) );
 writeLog ( "GET: " . var_export ( $_GET, true ) );
